ced1ea11dce57c52a1a018b6294ac0d584939c9f
hss70 <161866442+hss70@users.noreply.github.com>
2025-01-08 19:25:02 +0000

    added main.sql

---
+++ main.sql
@@ -0,0 +1,7 @@
+
+-- Single Table Lab Sheet
+
+CREATE TABLE pet (name VARCHAR(20), owner VARCHAR(20), species VARCHAR(20), sex CHAR(1), checkups SMALLINT UNSIGNED, birth DATE, death DATE);
+
+. schema
+. table
